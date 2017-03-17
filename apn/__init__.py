2d7b74f1fc6dd4be6ac4382819bd22168260a1fe
Eric P <proulx712@gmail.com>
2017-03-16 20:24:22 -0700

    updated config settings;

---
+++ apn/__init__.py
@@ -0,0 +1,32 @@
+# Copyright 2013 Donald Stufft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import absolute_import, division, print_function
+from __future__ import unicode_literals
+
+__all__ = (
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+)
+
+__title__ = "apn"
+__summary__ = "APN formatter tool to verify APN numbers"
+__uri__ = "https://github.com/dogpackdesign/apn-formats"
+
+__version__ = "0.0.1"
+
+__author__ = "Eric Proulx and Ken Harmon"
+__email__ = "contact@dogpackdesign.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2017 DogpackDesign"
--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import apn
 from setuptools import setup, find_packages
 
-setup(name='apn',
-      version='1.0',
-      description='APN format verification tool',
-      author='Eric Proulx and Ken Harmon',
-      packages=find_packages(),
+install_requires = [
+      "fuzzywuzzy >= 0.15.0",
+      "python-Levenshtein >= 0.12.0",
+]
+
+setup(name=apn.__title__,
+      version=apn.__version__,
+      description=apn.__summary__,
+      long_description=open("README.rst").read(),
+      license=apn.__license__,
+      url=apn.__uri__,
+
+      author=apn.__author__,
+      author_email=apn.__email__,
+      packages=["apn"],
       package_data={'apn': ['*.pkl']},
       )
