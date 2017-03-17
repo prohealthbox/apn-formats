128cafa30a3fc61eec5a2b3c17edb3ef40188bba
Eric P <proulx712@gmail.com>
2017-03-16 20:38:06 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
+from setuptools import setup
+
 import apn
-from setuptools import setup, find_packages
 
 install_requires = [
-      "fuzzywuzzy >= 0.15.0",
-      "python-Levenshtein >= 0.12.0",
+    "fuzzywuzzy >= 0.15.0",
+    "python-Levenshtein >= 0.12.0",
 ]
 
-setup(name=apn.__title__,
-      version=apn.__version__,
-      description=apn.__summary__,
-      long_description=open("README.rst").read(),
-      license=apn.__license__,
-      url=apn.__uri__,
+setup(
+    name=apn.__title__,
+    version=apn.__version__,
+    description=apn.__summary__,
+    long_description=open("README.rst").read(),
+    license=apn.__license__,
+    url=apn.__uri__,
 
-      author=apn.__author__,
-      author_email=apn.__email__,
-      packages=["apn"],
-      package_data={'apn': ['*.pkl']},
-      )
+    author=apn.__author__,
+    author_email=apn.__email__,
+    packages=["apn"],
+    package_data={'apn': ['*.pkl']},
+)
